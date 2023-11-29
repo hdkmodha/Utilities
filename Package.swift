bf502638a940ba5a50a62577f15f13c3234ecb7d
Hardik Modha <hardik.modha27@gmail.com>
2023-11-29 23:55:16 +0530

    Added other remote dependencies
    Added Alamofire and AlamofireImage

--- Package.swift
+++ Package.swift
@@ -5,15 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "Utilities",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    platforms: [.macOS(.v10_15), .iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(name: "Utilities", targets: ["Utilities"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.8.1")),
+        .package(url: "https://github.com/Alamofire/AlamofireImage.git", .upToNextMajor(from: "4.3.0")),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(name: "WebServiceManager", dependencies: []),
+        .target(name: "WebServiceManager", dependencies: ["Alamofire", "AlamofireImage"]),
         .target(name: "Utilities", dependencies: ["WebServiceManager"]),
     ]
 )
