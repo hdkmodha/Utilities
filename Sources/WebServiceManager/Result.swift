da5df6b89e3061263e0f0d58349c47bdd5f71912
Hardik Modha <hardik.modha27@gmail.com>
2023-11-29 23:40:02 +0530

    Added Result and reuseable view

---
+++ Sources/WebServiceManager/Result.swift
@@ -0,0 +1,31 @@
+//
+//  Result.swift
+//
+//
+//  Created by Hardik Modha on 29/11/23.
+//
+
+import Foundation
+
+public enum Result<Value> {
+    case success(Value)
+    case failure(Error)
+    
+    public var value: Value? {
+        switch self {
+        case .success(let value):
+            return value
+        case .failure:
+            return nil
+        }
+    }
+    
+    public var error: Error? {
+        switch self {
+        case .success:
+            return nil
+        case .failure(let error):
+            return error
+        }
+    }
+}
