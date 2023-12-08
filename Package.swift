75a758ec209d511a5992d1e0aafbdb2061151c6c
Hardik Modha <hardik.modha27@gmail.com>
2023-12-08 23:50:16 +0530

    Remove Support of WebService Manager

--- Package.swift
+++ Package.swift
@@ -10,14 +10,9 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(name: "Utilities", targets: ["Utilities"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.8.1")),
-        .package(url: "https://github.com/Alamofire/AlamofireImage.git", .upToNextMajor(from: "4.3.0")),
-    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(name: "WebServiceManager", dependencies: ["Alamofire", "AlamofireImage"]),
-        .target(name: "Utilities", dependencies: ["WebServiceManager"]),
+        .target(name: "Utilities", dependencies: []),
     ]
 )
