822592f016cc5ee99eeda0a9939ce609130f3816
Hardik Modha <hardik.modha27@gmail.com>
2024-05-05 12:25:49 +0530

    Update View+Extensions.swift
    make methdos pulbic

--- Sources/Utilities/View+Extensions.swift
+++ Sources/Utilities/View+Extensions.swift
@@ -8,10 +8,10 @@
 import Foundation
 import UIKit
 
-extension UIView {
+public extension UIView {
     
     //MARK: - Apply CornerRadius
-    func applyCornerRadius(withRadius radius: CGFloat) {
+    public func applyCornerRadius(withRadius radius: CGFloat) {
         self.layer.cornerRadius = radius
         self.clipsToBounds = true
     }
