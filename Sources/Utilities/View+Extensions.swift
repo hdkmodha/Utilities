226e43dbdc5c35493a74ebf4d575f0c9220995bc
Hardik Modha <hardik.modha27@gmail.com>
2024-05-05 12:24:10 +0530

    UIView+Extensions
    Added UIView extenstions file for helper functions

---
+++ Sources/Utilities/View+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  View+Extensions.swift
+//
+//
+//  Created by Hardik Modha on 05/05/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    //MARK: - Apply CornerRadius
+    func applyCornerRadius(withRadius radius: CGFloat) {
+        self.layer.cornerRadius = radius
+        self.clipsToBounds = true
+    }
+}
